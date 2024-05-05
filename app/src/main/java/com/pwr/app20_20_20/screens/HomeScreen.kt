ca33641fa6c29815c4cdb4dc11db191c040ef694
dawid splk <dawid.t.spalek@gmail.com>
2024-05-05 19:02:44 +0200

    add cycles info, fix Timer logic

--- app/src/main/java/com/pwr/app20_20_20/screens/HomeScreen.kt
+++ app/src/main/java/com/pwr/app20_20_20/screens/HomeScreen.kt
@@ -8,7 +8,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import com.pwr.app20_20_20.BottomNavigationBar
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
@@ -33,6 +37,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pwr.app20_20_20.viewmodels.TimerViewModel
 import kotlinx.coroutines.delay
 import kotlin.math.PI
+import kotlin.math.ceil
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -54,18 +59,26 @@ fun HomeScreen(navController: NavController, viewModel: TimerViewModel){
     {
         Column (
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+//            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
 
             val (mode, setMode) = remember { mutableStateOf("Focus") }
+            val (cycles, setCycles) = remember { mutableIntStateOf(numberOfCycles) }
 
             Text(
                 text = "$mode Mode",
                 color = Color.White,
-                fontSize = 30.sp,
+                fontSize = 40.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 15.dp, top = 50.dp)
+            )
+
+            Text(
+                text = "$cycles cycles left",
+                color = Color.White,
+                fontSize = 17.sp,
+                modifier = Modifier.padding(bottom = 150.dp)
             )
 
             Timer(
@@ -75,33 +88,36 @@ fun HomeScreen(navController: NavController, viewModel: TimerViewModel){
 //                focusTime = 3L * 1000L,
 //                restTime = 2L * 1000L,
 //                cycles = 4,
-                handleColorFocus = Color.Green,
-                handleColorRest = Color.Red,
-                activeBarColorFocus = Color(0xFF37B900),
+                handleColorFocus = Color(0xFF0FB04C),
+                handleColorRest = Color(0xFFFF9800),
+                activeBarColorFocus = Color(0xFF0FB04C),
                 activeBarColorRest = Color(0xFFFF9800),
                 modifier = Modifier.size(250.dp),
                 strokeWidth = 11.dp,
                 mode = mode,
-                setMode = setMode
+                setMode = setMode,
+                setCycles = setCycles
             )
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Timer(
     focusTime: Long,
     restTime: Long,
     cycles: Int,
-    handleColorFocus: Color = Color.Green,
+    handleColorFocus: Color = Color(0xFF0FB04C),
     handleColorRest: Color = Color.Red,
     inactiveBarColor: Color = Color.DarkGray,
-    activeBarColorFocus: Color = Color(0xFF37B900),
+    activeBarColorFocus: Color = Color(0xFF0FB04C),
     activeBarColorRest: Color = Color(0xFFF44336),
     modifier: Modifier = Modifier,
     strokeWidth: Dp = 5.dp,
     mode: String,
-    setMode: (String) -> Unit
+    setMode: (String) -> Unit,
+    setCycles: (Int) -> Unit
 ) {
     var size by remember { mutableStateOf(IntSize.Zero) }
     var value by remember { mutableStateOf(1f) }
@@ -121,9 +137,10 @@ fun Timer(
                 currentTime = restTime
                 setMode("Rest")
             } else {
+                currentCycle++
                 isFocusTime = true
                 currentTime = focusTime
-                currentCycle++
+                setCycles(cycles - currentCycle)
                 setMode("Focus")
             }
         } else if (currentCycle >= cycles) {
@@ -135,14 +152,23 @@ fun Timer(
         contentAlignment = Alignment.Center,
         modifier = modifier.onSizeChanged {
             size = it
-        }.clickable {
-            if (!isTimerRunning || currentTime <= 0L) {
-                isTimerRunning = true
-                currentTime = if (isFocusTime) focusTime else restTime
-            } else {
+        }.combinedClickable(
+            onClick = {
+                if (currentTime <= 0L) {
+                    currentTime = if (isFocusTime) focusTime else restTime
+                    currentCycle = 0
+                    setCycles(cycles - currentCycle)
+                }
                 isTimerRunning = !isTimerRunning
-            }
-        }
+            },
+            onLongClick = {
+                if(!isTimerRunning) {
+                    currentTime = focusTime
+                    currentCycle = 0
+                    setCycles(cycles)
+                }
+            },
+        )
     ) {
         Canvas(modifier = modifier) {
             drawArc(
@@ -190,7 +216,7 @@ fun Timer(
 }
 
 fun formatTime(timeInMillis: Long): String {
-    val totalSeconds = timeInMillis / 1000
+    val totalSeconds = ceil(timeInMillis.toDouble() / 1000).toLong()
     val minutes = totalSeconds / 60
     val seconds = totalSeconds % 60
     return if (minutes > 0) {
